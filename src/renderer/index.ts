6ea50ac73b745f203cd79b1765d037ad28cf2fe5
Wei Wei <shazhouke@gmail.com>
2025-02-05 11:01:36 +0800

    Move main/preload/renderer into separated folders

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,3 +1,4 @@
+
 /**
  * This file will automatically be loaded by webpack and run in the "renderer" context.
  * To learn more about the differences between the "main" and the "renderer" context in
