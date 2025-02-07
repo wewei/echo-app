c50371720df30373f09993d2019558f13e3db5dd
Wei Wei <shazhouke@gmail.com>
2025-02-07 14:04:01 +0800

    Add cache for useProfile

--- src/renderer/data/profile.ts
+++ src/renderer/data/profile.ts
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 import type { Profile } from "../../shared/types/profile";
+
+const profiles = new Map<string, Profile>();
 
 export const useProfile = (
   profileId: string
 ): [Profile | null, (updater: (prev: Profile) => Profile) => void] => {
-  const [profile, setProfile] = useState<Profile | null>(null);
+  const [profile, setProfile] = useState<Profile | null>(profiles.get(profileId));
   useEffect(() => {
     const loadProfile = async () => {
       const profile = await window.electron.profile.get(profileId);
+      profiles.set(profileId, profile);
       setProfile(profile);
     };
-    loadProfile();
+    if (!profile) {
+      loadProfile();
+    }
   }, [profileId]);
 
   return [
@@ -20,6 +26,7 @@ export const useProfile = (
       setProfile((prev) => {
         if (!prev) return prev;
         const newProfile = updater(prev);
+        profiles.set(profileId, newProfile);
         window.electron.profile.update(profileId, newProfile);
         return newProfile;
       });
@@ -57,4 +64,10 @@ export const useProfiles = (): [
       setProfiles(profiles.filter((p) => p.id !== profileId));
     },
   ];
+};
+
+export const useCurrentProfile = (): Profile | null => {
+  const profileId = useParams().profileId;
+  const [profile] = useProfile(profileId);
+  return profile;
 };
